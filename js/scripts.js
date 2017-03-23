a2a969ec9c42cac1ccb3339c92488dee81a59559
Nick Powell and Tanner Eustice <nick@>
2017-03-23 11:46:06 -0700

    add variables in js

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,15 @@
 $(function() {
-  var side1
+  $("#triangle form").submit(function(event) {
+      // ----this child way or straight in form
+    var side1 = parseInt($("input#side1").val ());
+    var side2 = parseInt($("input#side2").val ());
+    var side3 = parseInt($("input#side3").val ());
+    if (side1 === side2 && side1 === side3) {
+      $(".output").text("Equilateral")
+    }
+
+    event.preventDefault();
+  });
+
+
 });
