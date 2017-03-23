d17091e861f81e8502d3c74c586681967271220b
Nick Powell and Tanner Eustice <nick@>
2017-03-23 12:06:34 -0700

    add iso, scale, and equi triangles

--- js/scripts.js
+++ js/scripts.js
@@ -7,8 +7,19 @@ $(function() {
     if (side1 === side2 && side1 === side3) {
       $(".output").text("Equilateral")
     }
+    else if (side1 === side2 || side2 === side3 || side1 === side3) {
+      $(".output").text("Isosceles")
+    }
+    else if (side1 != side2 || side2 != side3 || side1 != side3) {
+      $(".output").text("Scalene")
+    }
+    else if (side1 + side2 >= side3 || side2 + side3 >= side1 || side1 + side3 >= side2) {
+      $(".output").text("NOT a triangle")
+    }
+
 
     event.preventDefault();
+
   });
 
 
