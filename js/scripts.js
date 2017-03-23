b06b73d7d09e4104fb3bddace98a41b110154aad
Nick Powell and Tanner Eustice <nick@>
2017-03-23 15:10:34 -0700

    fix bug, straight line triangle

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,17 @@
 $(function() {
   $("#triangle form").submit(function(event) {
       // ----this child way or straight in form
-    var side1 = parseInt($("input#side1").val ());
-    var side2 = parseInt($("input#side2").val ());
-    var side3 = parseInt($("input#side3").val ());
-    if (side1 === side2 && side1 === side3) {
+    var side1 = parseFloat($("input#side1").val ());
+    var side2 = parseFloat($("input#side2").val ());
+    var side3 = parseFloat($("input#side3").val ());
+
+    if (side1 <= 0 || side2 <= 0 || side3 <= 0) {
+      $(".output").text("That is NOT valid input. Please use positive numbers only.");
+    }
+    else if (side1 === side2 && side1 === side3) {
       $(".output").text("Equilateral");
     }
-    else if ((side1 === side2 && side1 + side2 >= side3) || (side2 === side3 && side2 + side3 >= side1) || (side1 === side3 && side1 + side3 >= side2)) {
+    else if ((side1 === side2 && side1 + side2 > side3) || (side2 === side3 && side2 + side3 > side1) || (side1 === side3 && side1 + side3 > side2)) {
       $(".output").text("Isosceles");
     }
     else if (side1 + side2 <= side3 || side2 + side3 <= side1 || side1 + side3 <= side2) {
@@ -17,7 +21,7 @@ $(function() {
       $(".output").text("Scalene");
     }
     else {
-      $(".output").text("That is NOT valid input. Please use numbers only.");
+      $(".output").text("That is NOT valid input. Please use positive numbers only.");
     }
 
 
